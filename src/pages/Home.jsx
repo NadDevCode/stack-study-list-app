b228c1740e6e4d4f48b8c94c1affd2a51864b3b6
Nad <aureliencartier@gmail.com>
2024-05-14 23:26:11 +0200

    Création de la page 'Home.jsx' avec ajout du texte. Création de la barre de navigation.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>stackStudyList</h1>
+            <div>Bienvenue sur votre plateforme d'assistance pour organiser votre apprentissage dans le domaine de la programmation.</div>
+        </div>
+    )
+}
