4d3f9d8654be2c0227759ec4d058ac301beea4f7
Nad <aureliencartier@gmail.com>
2024-05-16 17:50:32 +0200

    Routage des pages avec react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import TechnoAdd from'./pages/TechnoAdd';
 import TechnoList from './pages/TechnoList';
@@ -8,9 +10,11 @@ function App() {
   return (
     <>
       <Menu />
-      <Home />
-      <TechnoAdd />
-      <TechnoList />
+      <Routes>
+        <Route path='/' element={<Home /> } />
+        <Route path='/add' element={<TechnoAdd /> } />
+        <Route path='/list' element={<TechnoList />} />
+      </Routes>
     </>
   );   
 }
